9a02454d5b491f2b25bc6bbb7303d789d2cdd933
Renan Coelho <renandiett@gmail.com>
2018-07-02 03:58:57 -0300

    Add project files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+  document.querySelectorAll('section').forEach((element) => {
+    element.classList.add('show')
+  })
+
+  document.querySelector('main').classList.add('show')
+})
